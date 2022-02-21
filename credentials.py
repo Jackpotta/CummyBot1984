20d1626695291b06729e99f57916863a8d65a5a5
Jackpotta <matthieu.colin@live.fr>
2022-02-21 22:09:46 +0100

    Add subreddit name global var

--- credentials.py
+++ credentials.py
@@ -1,10 +1,10 @@
 def credentials():
     credentials = {
-        'username':'',
-		'password':'',
-		'client_id': '',
-		'client_secret': '',
-		'user_agent': '',
+        'username':'JeanDelaFontaineBot',
+		'password':'2d52CqRHnGeg',
+		'client_id': 'lM_0Yzy1lqAgww',
+		'client_secret': 'WOoyDkn8ufegn-0auOh-0xkKOvg',
+		'user_agent': 'web:copiepatesbot:v1.1 (by u/Jack_potta)',
         'channelid':'',
         'discordtoken':''
     }
